8452ae126d240d00b08db51a19056c2458d6728c
suprefuner <suprefuner@gmail.com>
2023-03-12 00:31:50 +0800

    fix some bug

--- server.js
+++ server.js
@@ -70,6 +70,9 @@ app.use(
     store: new MemoryStore({
       checkPeriod: 24 * 60 * 60,
     }),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+    },
     resave: false,
     saveUninitialized: false,
   })
